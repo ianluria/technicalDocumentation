770e4d90910dc7b9c7d7b82d12b82123f0a8946d
Ian Luria <ian.luria@gmail.com>
2019-01-10 22:29:50 -0700

    Cleaning up JS and HTML

--- techdoc.js
+++ techdoc.js
@@ -1,5 +1,3 @@
-
-
 const getSection = function (sectionID) {
 
     const mainDoc = document.getElementById("main-doc");
@@ -7,24 +5,6 @@ const getSection = function (sectionID) {
     return [...mainDoc.children].find(section => section.id === sectionID);
 }
 
-/*
-const applyToAll = function (CSSKey, CSSValue, optionalSection, opKey, opValue) {
-
-    //console.log(optionalSection);
-    const mainContentContainer = document.getElementById("main-content-container");
-
-    [...mainContentContainer.children].forEach(section => {
-
-        if (section === optionalSection) {
-            // section.style.setProperty(opKey, opValue);
-            section.classList.remove("hide");
-        } else {
-            // section.style.setProperty(CSSKey, CSSValue);
-            section.classList.add("hide");
-        }
-    });
-}
-*/
 //first, hide all sections except for the intro text
 
 const mainDoc = document.getElementById("main-doc");
@@ -38,8 +18,6 @@ const sectionDisplayContainer = document.getElementById("main-content-container"
     }
 });
 
-//sectionDisplayContainer.appendChild(getSection("intro-text"));
-
 const displaySection = function (section) {
 
     const displayContainer = document.getElementById("main-content-container");
@@ -52,7 +30,6 @@ const displaySection = function (section) {
 
     const duplicatedSection = section.cloneNode(true);
     duplicatedSection.classList.remove("hide");
-    // duplicatedSection.id = "duplicate";
     duplicatedSection.classList.add("duplicate");
 
     displayContainer.appendChild(duplicatedSection);
@@ -63,41 +40,8 @@ displaySection(getSection("intro-text"));
 [...document.getElementsByClassName("nav-link")].forEach(link => {
     link.addEventListener("click", (event) => {
         event.preventDefault();
-        //debugger;
         const sectionID = event.target.hash.match(/[^\#]/g).join('');
-        // console.log(sectionID);
-
 
         displaySection(getSection(sectionID));
     });
 });
-
-/*
-const breakText = function (event) {
-    //console.log(window.innerWidth);
-
-    if (window.innerWidth < 1200 && window.innerWidth > 900) {
-        const navBar = document.getElementById("navbar");
-        const navBarHeader = document.getElementById("navBarHeader");
-
-        const navBarWidth = navBar.offsetWidth;
-        const headerWidth = navBarHeader.offsetWidth;
-
-        let headerLinkInnerHTML = navBarHeader.firstElementChild.innerHTML;
-        //console.log("bar: ", navBarWidth, " header: ", headerWidth);
-        // debugger;
-
-        if (navBarWidth < headerWidth + 10) {
-            headerLinkInnerHTML = headerLinkInnerHTML.replace(/\s/, "<br>");
-            //console.log(navBarHeader.firstChild.innerHTML);
-            //console.log(headerLinkInnerHTML);
-        } else {
-            headerLinkInnerHTML = headerLinkInnerHTML.replace(/<br>/, " ");
-            //console.log("false");
-        }
-
-        navBarHeader.firstElementChild.innerHTML = headerLinkInnerHTML;
-    }
-}
-window.onresize = breakText;
-*/
